1d589769c0a02c95e651c21dbafe6daa255dae75
Đào Đức Mạnh <manhdao1006@gmail.com>
2023-09-07 21:52:13 +0700

    create-preface-intro-change-script

--- js/script.js
+++ js/script.js
@@ -5,30 +5,42 @@ var key_number_4 = document.getElementById("key_number_4");
 var key_number_5 = document.getElementById("key_number_5");
 var key_number_6 = document.getElementById("key_number_6");
 // script change page
-// start->key
+// start->preface
 document.getElementById("audio").onplay = ()=>{
     document.getElementById("cloud").style.visibility = "hidden";
     document.getElementById("start").style.visibility = "hidden";
-    document.getElementById("key").style.visibility = "visible";
+    document.getElementById("preface").style.visibility = "visible";
 
+}
+
+// preface-> key
+document.getElementById("next_key").onclick = ()=>{
+    document.getElementById("preface").style.visibility = "hidden";
+    document.getElementById("key").style.visibility = "visible";
 }
 
 // key->restart
 document.getElementById("back_restart").onclick = ()=>{
-    document.getElementById("key").style.visibility="hidden";
-    document.getElementById("restart").style.visibility="visible";
+    document.getElementById("key").style.visibility = "hidden";
+    document.getElementById("restart").style.visibility = "visible";
 }
 
 // restart->key
 document.getElementById("restart").onclick = ()=>{
-    document.getElementById("restart").style.visibility="hidden";
-    document.getElementById("key").style.visibility="visible";
+    document.getElementById("restart").style.visibility = "hidden";
+    document.getElementById("key").style.visibility = "visible";
+}
+
+// intro->B
+document.getElementById("next_content_B").onclick = ()=>{
+    document.getElementById("intro").style.visibility = "hidden";
+    document.getElementById("content_gift_B").style.visibility = "visible";
 }
 
 // B->key
 document.getElementById("back_key").onclick = ()=>{
+    document.getElementById("content_gift_B").style.visibility = "hidden";
     document.getElementById("key").style.visibility = "visible";
-    document.getElementById("intro_gift_B").style.visibility="hidden";
     key_number_1.value = "";
     key_number_2.value = "";
     key_number_3.value = "";
@@ -53,9 +65,9 @@ document.getElementById("go").onclick = ()=>{
     if(key_number_1.value == "3" && key_number_2.value == "0"
         && key_number_3.value == "6" && key_number_4.value == "6"
         && key_number_5.value == "1" && key_number_6.value == "0"){
-        // key->B
+        // key->intro
         document.getElementById("key").style.visibility="hidden";
-        document.getElementById("intro_gift_B").style.visibility="visible";
+        document.getElementById("intro").style.visibility="visible";
     } else if(key_number_1.value == "" || key_number_2.value == ""
         || key_number_3.value == "" || key_number_4.value == ""
         || key_number_5.value == "" || key_number_6.value == ""){
@@ -70,4 +82,36 @@ document.getElementById("go").onclick = ()=>{
         key_number_6.value = "";
     }
     
+}
+
+// show word B
+document.getElementById("word").addEventListener("mouseover", showWord);
+
+function showWord(event){
+    document.getElementById("up_here").style.display = "none";
+    document.getElementById("here").style.display = "none";
+    event.target.classList.remove("hide_word_B");
+    document.getElementById("minus").classList.remove("hide_minus_word_B");
+    document.getElementById("define").classList.remove("hide_define_B");
+    event.target.classList.add("show_word_B");
+    document.getElementById("minus").classList.add("show_minus_word_B");
+    document.getElementById("define").classList.add("show_define_B");
+}
+
+// show letter B
+var letter_B = document.getElementById("letter_B");
+var originContent = letter_B.innerHTML;
+var index = 1;
+
+document.getElementById("show_letter").onclick = ()=>{
+    letter_B.innerHTML = originContent.slice(0, 1);
+    document.getElementById("wait").style.display = "none";
+    document.getElementById("show_letter").style.cursor = "auto";
+    document.getElementById("word").style.cursor = "auto"
+    letter_B.classList.remove("hide_letter_content_B");
+    letter_B.classList.add("show_letter_content_B");
+    setInterval(()=>{
+        index++;
+        letter_B.innerHTML = originContent.slice(0, index);
+    }, 106)
 }
